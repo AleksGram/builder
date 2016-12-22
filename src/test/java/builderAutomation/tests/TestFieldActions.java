261c77d747248aef6e2543eafbd11aaa5e01da87
AleksGram <https://github.com/AleksGram/builder.git>
2016-12-22 12:05:57 +0200

    test

--- src/test/java/builderAutomation/tests/TestFieldActions.java
+++ src/test/java/builderAutomation/tests/TestFieldActions.java
@@ -9,6 +9,7 @@ public class TestFieldActions extends TestNgTestBase {
     @BeforeMethod
     public  void prepareActionsForTest() throws InterruptedException {
         prepareToTest("USA", "health", "m");
+        System.out.println(driver.getTitle());
     }
 
 
